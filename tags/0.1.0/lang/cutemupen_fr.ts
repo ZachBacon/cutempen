ea6b3c6dfa586398dadb667373d8215137a94c44
vknecht <vknecht@790847e5-28b2-490c-ab48-dc9e0705e8f8>
2010-12-29 10:57:05 +0000

    - Tagged 0.1.0

---
+++ tags/0.1.0/lang/cutemupen_fr.ts
@@ -0,0 +1,422 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>InputDialogClass</name>
+    <message>
+        <location filename="../inputdialog.ui" line="17"/>
+        <source>InputDialog</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../inputdialog.ui" line="29"/>
+        <source>Controler 1</source>
+        <translation>Contrôleur 1</translation>
+    </message>
+    <message>
+        <location filename="../inputdialog.ui" line="45"/>
+        <source>Controler 2</source>
+        <translation>Contrôleur 2</translation>
+    </message>
+    <message>
+        <location filename="../inputdialog.ui" line="61"/>
+        <source>Controler 3</source>
+        <translation>Contrôleur 3</translation>
+    </message>
+    <message>
+        <location filename="../inputdialog.ui" line="77"/>
+        <source>Controler 4</source>
+        <translation>Contrôleur 4</translation>
+    </message>
+    <message>
+        <location filename="../inputdialog.ui" line="98"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="20"/>
+        <source>CuteMupen</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="32"/>
+        <source>Run</source>
+        <translation>Lancer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="39"/>
+        <source>Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="81"/>
+        <source>Paths</source>
+        <translation>Chemins</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="89"/>
+        <location filename="../mainwindow.ui" line="103"/>
+        <location filename="../mainwindow.ui" line="110"/>
+        <location filename="../mainwindow.ui" line="117"/>
+        <location filename="../mainwindow.ui" line="486"/>
+        <source>Browse...</source>
+        <translation>Parcourir...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="96"/>
+        <source>M64+ library file</source>
+        <translation>Fichier bibliothèque M64+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="124"/>
+        <source>M64+ plugins directory</source>
+        <translation>Répertoire plugins M64+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="131"/>
+        <source>M64+ data directory</source>
+        <translation>Répertoire données M64+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="138"/>
+        <source>M64+ config directory</source>
+        <translation>Répertoire config M64+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="173"/>
+        <source>Settings</source>
+        <translation>Règlages</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="181"/>
+        <source>Plugins</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Graphic</source>
+        <translation type="obsolete">Graphique</translation>
+    </message>
+    <message>
+        <source>Sound</source>
+        <translation type="obsolete">Son</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="240"/>
+        <source>Input</source>
+        <translation>Entrées (input)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="247"/>
+        <source>Audio</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="254"/>
+        <source>RSP</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="347"/>
+        <source>Emulator Mode</source>
+        <translation>Mode d&apos;émulation</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="353"/>
+        <source>Pure interpreter</source>
+        <translation>Interprèteur pur</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="360"/>
+        <source>Cached interpreter</source>
+        <translation>Interprèteur &quot;caché&quot;</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="367"/>
+        <source>Dynamic recompiler</source>
+        <translation>Recompilateur dynamique</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="399"/>
+        <source>Logging</source>
+        <translation>Journalisation</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="407"/>
+        <source>Disabled</source>
+        <translation>Désactivé</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="417"/>
+        <source>Enabled</source>
+        <translation>Activé</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="427"/>
+        <source>Verbose</source>
+        <translation>Verbeux</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="272"/>
+        <source>Graphics</source>
+        <translation>Graphisme
+</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="288"/>
+        <source>640x480</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="293"/>
+        <source>720x480</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="298"/>
+        <source>800x600</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="303"/>
+        <source>1024x768</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="318"/>
+        <source>On Screen Display</source>
+        <translation>Affichage en incrustation</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="311"/>
+        <source>Fullscreen</source>
+        <translation>Plein-écran</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="227"/>
+        <source>Video</source>
+        <translation>Vidéo</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="237"/>
+        <source>Input settings are not supported yet.</source>
+        <translation>Le règlage des entrées ne fonctionne pas encore.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="468"/>
+        <source>ROMs</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="476"/>
+        <source>ROMs directory</source>
+        <translation>Répertoire de ROMs</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="524"/>
+        <source>Logs</source>
+        <translation>Journal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="530"/>
+        <source>Copy</source>
+        <translation>Copier</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="537"/>
+        <source>Clear</source>
+        <translation>Effacer</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="561"/>
+        <source>About</source>
+        <translation>A propos</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="622"/>
+        <source>qrc:/about/resources/about.html</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../m64pcom.cpp" line="77"/>
+        <source>PluginGetVersion</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../m64pcom.cpp" line="78"/>
+        <source>Couldn&apos;t find PluginGetVersion()</source>
+        <translation>Impossible de trouver PluginGetVersion()</translation>
+    </message>
+    <message>
+        <location filename="../m64pcom.cpp" line="150"/>
+        <source>Calling CoreStartup()</source>
+        <translation>Appel de CoreStartup()</translation>
+    </message>
+    <message>
+        <location filename="../m64pcom.cpp" line="151"/>
+        <source>Error starting Mupen64Plus core library.</source>
+        <translation>Erreur lors du démarrage de la bibliothèque Mupen64Plus core.</translation>
+    </message>
+    <message>
+        <location filename="../m64pcom.cpp" line="160"/>
+        <source>Loading Configuration</source>
+        <translation>Chargement de la configuration</translation>
+    </message>
+    <message>
+        <location filename="../m64pcom.cpp" line="161"/>
+        <source>Problem while loading configuration !</source>
+        <translation>Problême lors du chargement de la configuration !</translation>
+    </message>
+    <message>
+        <location filename="../m64pcom.cpp" line="175"/>
+        <source>Error opening ROM file</source>
+        <translation>Erreur lors de l&apos;ouverture du fichier ROM</translation>
+    </message>
+    <message>
+        <location filename="../m64pcom.cpp" line="176"/>
+        <source>Error while trying to load ROM file:
+</source>
+        <translation>Erreur lors de la tentative d echargement du fichier ROM :</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="120"/>
+        <source>Core library not ready</source>
+        <translation>Bibliothèque core non prete</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="121"/>
+        <source>Did you select M64+ library file in Paths tab ?</source>
+        <translation>Avez-vous sélectionné le fichier bibliothèque M64+ dans l&apos;onglet Chemins ?</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="130"/>
+        <source>No ROM selected</source>
+        <translation>Pas de ROM sélectionnée</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="131"/>
+        <source>Please select a ROM file.</source>
+        <translation>Veuillez sélectionner un fichier ROM.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="139"/>
+        <source>Error loading ROM</source>
+        <translation>Erreur de chargement de la ROM</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="140"/>
+        <source>Unable to load ROM !</source>
+        <translation>Impossible de charge la ROM !</translation>
+    </message>
+    <message>
+        <source>Error searching for plugins</source>
+        <translation type="obsolete">Erreur lors de la recherche de plugins</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="43"/>
+        <source>Select Mupen64 core library file</source>
+        <translation>Sélectionnez le fichier bibliothèque Mupen64 core</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="54"/>
+        <source>File does not exist</source>
+        <translation>Le fichier n&apos;existe pas</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="55"/>
+        <source>Please set the library file to a valid file.</source>
+        <translation>Veuillez choisir un fichier bibliothèque valide.</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="103"/>
+        <source>Select Mupen64 plugins directory</source>
+        <translation>Sélectionnez le répertoire des plugins Mupen64</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="114"/>
+        <location filename="../paths.cpp" line="186"/>
+        <location filename="../paths.cpp" line="223"/>
+        <location filename="../paths.cpp" line="260"/>
+        <source>Directory does not exist</source>
+        <translation>Le répertoire n&apos;existe pas</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="115"/>
+        <source>Please set the plugins directory to a valid directory.</source>
+        <translation>Veuillez choisir un répertoire de plugins valide.</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="172"/>
+        <source>Select data directory</source>
+        <translation>Sélectionnez le répertoire de données</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="187"/>
+        <source>Please set the data directory to a valid directory.</source>
+        <translation>Veuillez choisir un répertoire de données valide.</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="209"/>
+        <source>Select configuration directory</source>
+        <translation>Sélectionnez un répertoire de configuration</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="224"/>
+        <source>Please set the configuration directory to a valid directory.</source>
+        <translation>Veuillez choisir un répertoire de configuration valide.</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="246"/>
+        <source>Select ROMs directory</source>
+        <translation>Sélectionnez le répertoire des ROMs</translation>
+    </message>
+    <message>
+        <location filename="../paths.cpp" line="261"/>
+        <source>Please set the ROMs directory to a valid directory.</source>
+        <translation>Veuillez choisir un répertoire de ROMs valide.</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="116"/>
+        <source>Invalid resolution</source>
+        <translation>Résolution invalide</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="117"/>
+        <source>This resolution is invalid: </source>
+        <translation>Cette résolution est invalide : </translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="224"/>
+        <source>Unknown EmuMode setting</source>
+        <translation>Règlage EmuMode inconnu</translation>
+    </message>
+    <message>
+        <location filename="../settings.cpp" line="225"/>
+        <source>Invalid value for EmuMode</source>
+        <translation>Valeur pour EmuMode invalide</translation>
+    </message>
+</context>
+<context>
+    <name>PluginDialogClass</name>
+    <message>
+        <location filename="../plugindialog.ui" line="17"/>
+        <source>Plugin Settings</source>
+        <translation>Règlages des plugins</translation>
+    </message>
+    <message>
+        <location filename="../plugindialog.ui" line="33"/>
+        <source>Close</source>
+        <translation>Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>QuaZipFile</name>
+    <message>
+        <location filename="../quazip/quazipfile.cpp" line="141"/>
+        <source>ZIP/UNZIP API error %1</source>
+        <translation>Erreur API ZIP/UNZIP %1</translation>
+    </message>
+</context>
+</TS>
